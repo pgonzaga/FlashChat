45d166e5c469821c9144e3feb4132ad6d5457a42
Pablo Gonzaga <pgonzaga.uy@gmail.com>
2018-04-14 17:06:44 -0300

    Adds spinners on registration and login pages

--- Flash Chat/LogInViewController.swift
+++ Flash Chat/LogInViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 
 class LogInViewController: UIViewController {
@@ -26,12 +27,15 @@ class LogInViewController: UIViewController {
 
    
     @IBAction func logInPressed(_ sender: AnyObject) {
+        SVProgressHUD.show()
+
         Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
 
             if error != nil {
                 print("error")
             } else {
                 self.performSegue(withIdentifier: "goToChat", sender: self)
+                SVProgressHUD.dismiss()
             }
         }
     }
--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 
 class RegisterViewController: UIViewController {
@@ -29,11 +30,13 @@ class RegisterViewController: UIViewController {
 
   
     @IBAction func registerPressed(_ sender: AnyObject) {
+        SVProgressHUD.show()
         Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
             if error != nil {
                 print("error")
             } else {
                 self.performSegue(withIdentifier: "goToChat", sender: self)
+                SVProgressHUD.dismiss()
             }
         }
         
